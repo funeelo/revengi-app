f2c2dacb152bfdb09e0f6696e1c45b08e1e3415a
Abhi <85984486+AbhiTheModder@users.noreply.github.com>
2025-05-15 02:56:11 +0530

    update api base url

--- lib/dio.dart
+++ lib/dio.dart
@@ -2,7 +2,7 @@ import 'package:dio/dio.dart';
 
 final dio = Dio(
   BaseOptions(
-    baseUrl: 'https://api.example.net',
+    baseUrl: 'https://api.revengi.in',
     connectTimeout: Duration(seconds: 5),
     receiveTimeout: Duration(seconds: 30),
     contentType: 'application/json',
